650bddd067ce85feb7792204115fdf849575e2d3
Henrik Cheng <henrikcheng@live.se>
2021-03-17 10:43:54 +0100

    init state 0 again

--- src/App.js
+++ src/App.js
@@ -3,9 +3,16 @@ import "./App.css";
 import React from "react";
 import { render } from "react-dom";
 import Counter from "./Counter";
-import { createStore } from 'redux';
+import { createStore } from "redux";
 
-const store = createStore();
+const initialstate = { count: 0 };
+
+function reducer(state = initialstate, action) {
+  console.log("reducer", state, action);
+  return state;
+}
+
+const store = createStore(reducer);
 
 function App() {
   return (
