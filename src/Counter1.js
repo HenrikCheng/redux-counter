830b131d7aaa139928a73668d98cf1499a315544
Henrik Cheng <henrikcheng@live.se>
2021-03-17 14:14:49 +0100

    added action constants

--- src/Counter1.js
+++ src/Counter1.js
@@ -1,18 +1,14 @@
 import React from "react";
 import { connect } from "react-redux";
-import { INCREMENT, DECREMENT, RESET } from "./actions";
+import { increment, decrement } from "./actions";
 
 class Counter extends React.Component {
   increment = () => {
-    this.props.dispatch({ type: INCREMENT });
+    this.props.dispatch({ type: "INCREMENT" });
   };
 
   decrement = () => {
-    this.props.dispatch({ type: DECREMENT });
-  };
-
-  reset = () => {
-    this.props.dispatch({ type: RESET });
+    this.props.dispatch({ type: "DECREMENT" });
   };
 
   render() {
@@ -24,7 +20,6 @@ class Counter extends React.Component {
           <span className="count">{this.props.count}</span>
           <button onClick={this.increment}>+</button>
         </div>
-        <span onClick={this.reset}>Reset</span>
       </div>
     );
   }
